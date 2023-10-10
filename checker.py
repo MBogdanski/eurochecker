8dfd82331a5f06a5cebb5cec18bbe6ccd93f6e33
MBogdanski <dertara@gmail.com>
2023-10-10 09:34:12 +0200

    Introduce notifications about items availibility

--- checker.py
+++ checker.py
@@ -1,6 +1,9 @@
 import csv
 import datetime
+import os
 from dataclasses import dataclass
+from email.message import EmailMessage
+from smtplib import SMTP
 
 import requests
 from bs4 import BeautifulSoup
@@ -13,32 +16,45 @@ class AgdItem:
     price: int = 0
 
 
-agd_class_items = [
-    AgdItem('lodowka', 'https://www.euro.com.pl/lodowki/samsung-rb38t705cb.bhtml'),
-    AgdItem('suszarka', 'https://www.euro.com.pl/suszarki/bosch-serie-6-wtw8760epl.bhtml'),
-    AgdItem('ekspres', 'https://www.euro.com.pl/ekspresy-cisnieniowe/delonghi-dinamica-plus-ecam-370-70-b.bhtml'),
-    AgdItem('indukcja', 'https://www.euro.com.pl/plyty-do-zabudowy/electrolux-eiv835.bhtml'),
-    AgdItem('pralka', 'https://www.euro.com.pl/pralki/samsung-addwash-ww80t554dae.bhtml'),
-    AgdItem('piekarnik', 'https://www.euro.com.pl/piekarniki-do-zabudowy/electrolux-coc8h31z.bhtml'),
-    # AgdItem('odkurzacz', 'https://www.euro.com.pl/odkurzacze-pionowe/samsung-jet-90-premium-vs20r9048t3-ge.bhtml'),
-    AgdItem('zmywarka', 'https://www.euro.com.pl/zmywarki-do-zabudowy/electrolux-zmywarka-eem48321l-electrolux.bhtml'),
-    # AgdItem('rafalek', 'https://www.euro.com.pl/odkurzacze-automatyczne/beko-vrr81214vw.bhtml'),
-    AgdItem('okap', 'https://www.euro.com.pl/okapy/electrolux-cfg516r.bhtml')
+agd_items = [
+    AgdItem('odkurzacz', 'https://www.euro.com.pl/odkurzacze-pionowe/samsung-jet-90-premium-vs20r9048t3-ge.bhtml'),
+    AgdItem('rafalek', 'https://www.euro.com.pl/odkurzacze-automatyczne/beko-vrr81214vw.bhtml')
 ]
 
 product_prices = {}
 
 
-def log_price(product_name, price):
+def log_agd_info(product_name, price, availability):
     timestamp = datetime.datetime.now()
     with open('agd_prices.csv', mode='a', newline='') as file:
         writer = csv.writer(file)
-        writer.writerow([timestamp, product_name, price])
+        writer.writerow([timestamp, product_name, price, availability])
+
+
+def notify(subject, body, receiver):
+    user = os.environ['EMAIL']
+    password = os.environ['PASSWORD']
+
+    msg = EmailMessage()
+    msg.set_content(body)
+    msg['subject'] = subject
+    msg['to'] = receiver
+    msg['from'] = user
+
+    server = SMTP("smtp.gmail.com", 587)
+    server.starttls()
+    server.login(user, password)
+
+    try:
+        server.sendmail(user, receiver, msg.as_string())
+    finally:
+        print(f'Message was send to {receiver}')
+        server.quit()
 
 
 def fetch_agd_prices():
     price_sum = 0
-    for agd in agd_class_items:
+    for agd in agd_items:
         req = requests.get(agd.url)
         soup = BeautifulSoup(req.text, 'html.parser')
         prices = soup.find_all('span', 'price-template__large--total')
@@ -47,7 +63,12 @@ def fetch_agd_prices():
             actual_price = actual_price * 2
         price_sum = price_sum + actual_price
         print(f'{agd.name} have actual price of {actual_price}')
-        log_price(agd.name, actual_price)
+        available = is_available(soup)
+        if available:
+            notify(f'{agd.name} agd item is available now',
+                   f'Item is available with {actual_price} price, under {agd.url} url',
+                   os.environ['RECIPIENT'])
+        log_agd_info(agd.name, actual_price, available)
     print(f'Sum of all agd items: {price_sum}')
 
 
@@ -56,7 +77,7 @@ def search_min_max_prices():
         csv_reader = csv.reader(file)
         next(csv_reader)
         for row in csv_reader:
-            timestamp, agd_name, price = row
+            timestamp, agd_name, price, available = row
             price = int(price)
 
             if agd_name not in product_prices:
@@ -73,6 +94,11 @@ def show_min_max_prices():
         min_price = prices['min_price']
         max_price = prices['max_price']
         print(f"Product: {product_name}, Min Price: {min_price}, Max Price: {max_price}")
+
+
+def is_available(soup: BeautifulSoup) -> bool:
+    add_cart_button = soup.find('button', attrs={'data-test', 'add-product-to-the-cart'})
+    return add_cart_button is not None
 
 
 if __name__ == '__main__':
